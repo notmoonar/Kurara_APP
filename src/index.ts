c7509a715a6b7020c6710087dcd2f17b8f22d47b
notmoonar <franciscogabriel0360@gmail.com>
2025-04-22 17:56:25 -0300

    feat: add ESLint configuration and update package.json
    - Created a new ESLint configuration file with custom rules for code quality.
    - Added ESLint as a development dependency in package.json.
    - Updated package.json to specify module type and entry point.
    - Removed existing code in src/index.ts as part of a refactor.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const dotenv = require('dotenv');
+dotenv.config();
 
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-})
-
-client.on('message', (message: { content: string; channel: { send: (arg0: string) => void } }) => {
-    if (message.content === '!ping') {  // Check if the message is "!ping"
-        message.channel.send('Pong!')  // Reply with "Pong!"
-    }});
-
-client.login(process.env.DISCORD_BOT_TOKEN)
+console.log(process.env.TOKEN);
